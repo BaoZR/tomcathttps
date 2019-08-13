08bdca8423fdf439b2c1c83eb94e884f48d39b80
BaoZhengRan <474234165@qq.com>
2019-08-13 15:16:01 +0800

    Initial commit 加入一个证书，在application.properties里配置一下就可以用了

---
+++ src/main/java/com/bob/tomcathttps/TomcathttpsApplication.java
@@ -0,0 +1,13 @@
+package com.bob.tomcathttps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcathttpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcathttpsApplication.class, args);
+    }
+
+}
